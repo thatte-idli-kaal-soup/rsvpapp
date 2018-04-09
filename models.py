8c7a5d0a1839ac2ac70e3c0f2a6b228ba7bdcd16
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-04-09 14:50:45 +0530

    Add timestamp to RSVPs
    Closes #4

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask_mongoengine import MongoEngine
 
 from utils import random_id
@@ -10,6 +12,7 @@ class RSVP(db.EmbeddedDocument):
     name = db.StringField(unique=True)
     email = db.EmailField()
     note = db.StringField()
+    date = db.DateTimeField(required=True, default=datetime.datetime.now)
 
 
 class Event(db.Document):
