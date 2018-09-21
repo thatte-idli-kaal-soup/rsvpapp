5813233cd016d35cacbc05d78c07ba86df222f87
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-09-21 12:16:32 +0530

    format: Format gdrive_utils using latest version of black

--- rsvp/gdrive_utils.py
+++ rsvp/gdrive_utils.py
@@ -8,84 +8,88 @@ from google.oauth2 import service_account
 import requests
 
 # Local library
-SERVICE_ACCOUNT_FILE = 'service_account_file.json'
+SERVICE_ACCOUNT_FILE = "service_account_file.json"
 
 
 def download_service_account_file():
-    url = os.environ['GOOGLE_SERVICE_ACCOUNT_FILE_URL']
-    with open(SERVICE_ACCOUNT_FILE, 'w') as f:
+    url = os.environ["GOOGLE_SERVICE_ACCOUNT_FILE_URL"]
+    with open(SERVICE_ACCOUNT_FILE, "w") as f:
         json.dump(requests.get(url).json(), f)
 
 
 def create_service():
     if not os.path.exists(SERVICE_ACCOUNT_FILE):
         download_service_account_file()
-    SCOPES = ['https://www.googleapis.com/auth/drive']
+    SCOPES = ["https://www.googleapis.com/auth/drive"]
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES
     )
-    service = build('drive', 'v3', credentials=credentials)
+    service = build("drive", "v3", credentials=credentials)
     return service
 
 
 def update_permissions(service, file_id, emails):
-    permissions = service.permissions().list(
-        fileId=file_id, fields='permissions(id, emailAddress, role)'
-    ).execute()
+    permissions = (
+        service.permissions()
+        .list(fileId=file_id, fields="permissions(id, emailAddress, role)")
+        .execute()
+    )
     permissions = [
         permission
-        for permission in permissions.get('permissions', [])
-        if 'emailAddress' in permission
+        for permission in permissions.get("permissions", [])
+        if "emailAddress" in permission
     ]
     permission_emails = {
-        permission['emailAddress'].lower() for permission in permissions
+        permission["emailAddress"].lower() for permission in permissions
     }
     delete_permissions = [
         permission
         for permission in permissions
-        if permission['emailAddress'].lower() not in emails
-        and permission['role'] != 'owner'
-        and 'gserviceaccount.com' not in permission['emailAddress']
+        if permission["emailAddress"].lower() not in emails
+        and permission["role"] != "owner"
+        and "gserviceaccount.com" not in permission["emailAddress"]
     ]
     new_emails = [email for email in emails if email not in permission_emails]
-    print('Adding {} permissions'.format(len(new_emails)))
-    print('\n'.join(new_emails))
+    print("Adding {} permissions".format(len(new_emails)))
+    print("\n".join(new_emails))
     for email in new_emails:
         body = {
-            'type': 'user',
-            'role': 'writer',
-            'emailAddress': email,
-            'sendNotificationEmail': False,
+            "type": "user",
+            "role": "writer",
+            "emailAddress": email,
+            "sendNotificationEmail": False,
         }
         service.permissions().create(fileId=file_id, body=body).execute()
-    print('Deleting {} permissions'.format(len(delete_permissions)))
+    print("Deleting {} permissions".format(len(delete_permissions)))
     print(
-        '\n'.join(
-            permission['emailAddress'] for permission in delete_permissions
+        "\n".join(
+            permission["emailAddress"] for permission in delete_permissions
         )
     )
     for permission in delete_permissions:
         service.permissions().delete(
-            fileId=file_id, permissionId=permission['id']
+            fileId=file_id, permissionId=permission["id"]
         ).execute()
 
 
 def photos(service, root):
     q = "'{}' in parents and mimeType contains 'image/'"
     for sub_dir in walk_dir(service, root):
-        photos = service.files().list(
-            q=q.format(sub_dir['id']),
-            fields='files(id, imageMediaMetadata)',
-            pageSize=1000,
-        ).execute()[
-            'files'
-        ]
+        photos = (
+            service.files()
+            .list(
+                q=q.format(sub_dir["id"]),
+                fields="files(id, imageMediaMetadata)",
+                pageSize=1000,
+            )
+            .execute()["files"]
+        )
         yield from [
             {
-                'gdrive_parent': sub_dir['id'],
-                'gdrive_parent_name': sub_dir['name'],
-                'gdrive_id': photo['id'],
-                'gdrive_metadata': photo['imageMediaMetadata'],
+                "gdrive_parent": sub_dir["id"],
+                "gdrive_parent_name": sub_dir["name"],
+                "gdrive_id": photo["id"],
+                "gdrive_metadata": photo["imageMediaMetadata"],
             }
             for photo in photos
         ]
@@ -93,12 +97,12 @@ def photos(service, root):
 
 def list_sub_dirs(service, root):
     q = "'{}' in parents and mimeType='{}'"
-    mime_type = 'application/vnd.google-apps.folder'
-    sub_dirs = service.files().list(
-        q=q.format(root, mime_type), fields='files(id, name)'
-    ).execute()[
-        'files'
-    ]
+    mime_type = "application/vnd.google-apps.folder"
+    sub_dirs = (
+        service.files()
+        .list(q=q.format(root, mime_type), fields="files(id, name)")
+        .execute()["files"]
+    )
     yield from sub_dirs
 
 
@@ -107,4 +111,4 @@ def walk_dir(service, root):
     yield from sub_dirs
 
     for sub_dir in sub_dirs:
-        yield from walk_dir(service, sub_dir['id'])
+        yield from walk_dir(service, sub_dir["id"])
