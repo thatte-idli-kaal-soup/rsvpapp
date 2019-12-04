ca8024f23d765d85f3f04bb31b5c18e0d1247a47
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-12-04 09:39:20 +0530

    Upgrade to the latest Full Calendar version
    Also, switch to a list view from the grid view

--- rsvp/static/calendar.js
+++ rsvp/static/calendar.js
@@ -1,16 +1,36 @@
-var getEventColor = function(data) {
-    if (data.cancelled) {
-        return 'red';
-    } else if (data.archived) {
-        return 'grey';
-    }
-};
 $(function() {
-    // page is now ready, initialize the calendar...
-    $('#calendar').fullCalendar({
-        themeSystem: 'bootstrap4',
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['list', 'bootstrap'],
+        defaultView: 'listMonth',
         events: '/api/events/',
+        themeSystem: 'bootstrap',
+        timeZone: 'UTC',
         editable: false,
+        noEventsMessage: 'No Events',
+        contentHeight: 'auto',
+        listDayAltFormat: false,
+        listDayFormat: { month: 'short', year: 'numeric', day: 'numeric', weekday: 'long' },
+        views: {
+            listYear: { buttonText: 'Year' },
+            listMonth: { buttonText: 'Month' }
+        },
+        customButtons: {
+            addNew: {
+                text: 'Add Event',
+                click: function() {
+                    document.location.href = '/new_event';
+                }
+            }
+        },
+        header: {
+            left: 'title',
+            center: '',
+            right: 'prev,listMonth,listYear,next'
+        },
+        footer: {
+            right: 'addNew'
+        },
         eventDataTransform: function(data) {
             var event = {
                 id: data._id.$oid,
@@ -18,20 +38,11 @@ $(function() {
                 start: data.date.$date,
                 end: data._end_date ? data._end_date.$date : undefined,
                 url: `/event/${data._id.$oid}`,
-                color: getEventColor(data)
+                classNames: [data.cancelled ? 'cancelled-event' : '']
             };
             return event;
-        },
-        footer: {
-            right: 'addNew'
-        },
-        customButtons: {
-            addNew: {
-                text: 'Add Event',
-                click: function() {
-                    document.location.href = "{{url_for('event_editor')}}";
-                }
-            }
         }
     });
+
+    calendar.render();
 });
