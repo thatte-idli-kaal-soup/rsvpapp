43352e3efd41292eddb774cc49ae166c7dfed6ea
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-06-27 00:04:46 +0530

    splitwise: Extract some of the utils to a separate module

---
+++ rsvp/splitwise_utils.py
@@ -0,0 +1,61 @@
+import os
+
+from flask import _app_ctx_stack as stack
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+from oauthlib.oauth2 import WebApplicationClient, BackendApplicationClient
+import requests
+from werkzeug.contrib.cache import SimpleCache
+
+
+SPLITWISE_BASE_URL = "https://secure.splitwise.com/"
+SPLITWISE_TOKEN = os.environ.get("SPLITWISE_TOKEN")
+AUTH_HEADERS = {"Authorization": f"Bearer {SPLITWISE_TOKEN}"}
+CACHE = SimpleCache()
+
+
+class HybridClient(WebApplicationClient):
+    """Override grant type on WebApplicationClient.
+
+    Splitwise expects client_credentials grant_type, but WebApplicationClient
+    has application_code grant_type.
+
+    """
+
+    grant_type = BackendApplicationClient.grant_type
+
+
+def make_splitwise_blueprint(client_id=None, client_secret=None):
+    client = HybridClient(client_id, token=None)
+    splitwise_bp = OAuth2ConsumerBlueprint(
+        "splitwise",
+        __name__,
+        client_id=client_id,
+        client_secret=client_secret,
+        client=client,
+        base_url=SPLITWISE_BASE_URL,
+        token_url=f"{SPLITWISE_BASE_URL}/oauth/token",
+        authorization_url=f"{SPLITWISE_BASE_URL}/oauth/authorize",
+        # NOTE: Hack to include the argument to OAuth2Session().fetch_token
+        token_url_params={"include_client_id": True},
+    )
+
+    @splitwise_bp.before_app_request
+    def set_applocal_session():
+        ctx = stack.top
+        ctx.splitwise_oauth = splitwise_bp.session
+
+    return splitwise_bp
+
+
+def get_groups():
+    cache_key = "splitwise_groups"
+    groups = CACHE.get(cache_key)
+    if groups is None:
+        url = f"{SPLITWISE_BASE_URL}/api/v3.0/get_groups"
+        response = requests.get(url, headers=AUTH_HEADERS)
+        assert response.status_code == 200, "Could not fetch groups for user."
+        groups = response.json().get("groups", [])
+        # Ignore direct transactions group which has the ID 0
+        groups = [group for group in groups if group["id"] != 0]
+        CACHE.set(cache_key, groups)  # default_timeout = 5mins
+    return groups
--- rsvp/splitwise_views.py
+++ rsvp/splitwise_views.py
@@ -2,72 +2,17 @@ from functools import partial
 import os
 
 from flask import redirect, url_for, _app_ctx_stack as stack, flash, request, session
-from flask_dance.consumer import OAuth2ConsumerBlueprint, oauth_authorized
-from flask_login import current_user, login_required
+from flask_dance.consumer import oauth_authorized
 from flask.globals import LocalProxy, _lookup_app_object
-from oauthlib.oauth2 import WebApplicationClient, BackendApplicationClient
+from flask_login import current_user, login_required
 import requests
-from werkzeug.contrib.cache import SimpleCache
 
 from .app import app
 from .models import Event, ANONYMOUS_EMAIL, User
-
-SPLITWISE_BASE_URL = "https://secure.splitwise.com/"
-SPLITWISE_TOKEN = os.environ.get("SPLITWISE_TOKEN")
-AUTH_HEADERS = {"Authorization": f"Bearer {SPLITWISE_TOKEN}"}
-CACHE = SimpleCache()
-
-
-class HybridClient(WebApplicationClient):
-    """Override grant type on WebApplicationClient.
-
-    Splitwise expects client_credentials grant_type, but WebApplicationClient
-    has application_code grant_type.
-
-    """
-
-    grant_type = BackendApplicationClient.grant_type
-
-
-def make_splitwise_blueprint(client_id=None, client_secret=None):
-    client = HybridClient(client_id, token=None)
-    splitwise_bp = OAuth2ConsumerBlueprint(
-        "splitwise",
-        __name__,
-        client_id=client_id,
-        client_secret=client_secret,
-        client=client,
-        base_url=SPLITWISE_BASE_URL,
-        token_url=f"{SPLITWISE_BASE_URL}/oauth/token",
-        authorization_url=f"{SPLITWISE_BASE_URL}/oauth/authorize",
-        # NOTE: Hack to include the argument to OAuth2Session().fetch_token
-        token_url_params={"include_client_id": True},
-    )
-
-    @splitwise_bp.before_app_request
-    def set_applocal_session():
-        ctx = stack.top
-        ctx.splitwise_oauth = splitwise_bp.session
-
-    return splitwise_bp
-
-
-def get_groups():
-    cache_key = "splitwise_groups"
-    groups = CACHE.get(cache_key)
-    if groups is None:
-        url = f"{SPLITWISE_BASE_URL}/api/v3.0/get_groups"
-        response = requests.get(url, headers=AUTH_HEADERS)
-        assert response.status_code == 200, "Could not fetch groups for user."
-        groups = response.json().get("groups", [])
-        # Ignore direct transactions group which has the ID 0
-        groups = [group for group in groups if group["id"] != 0]
-        CACHE.set(cache_key, groups)  # default_timeout = 5mins
-    return groups
+from .splitwise_utils import make_splitwise_blueprint, SPLITWISE_BASE_URL, AUTH_HEADERS
 
 
 splitwise = LocalProxy(partial(_lookup_app_object, "splitwise_oauth"))
-
 splitwise_blueprint = make_splitwise_blueprint(
     client_id=os.environ.get("SPLITWISE_KEY"),
     client_secret=os.environ.get("SPLITWISE_SECRET"),
